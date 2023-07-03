e41d55b00ed0b85a7d15cb3adac11e6628a894ee
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-07-03 15:52:16 +0200

    Update LinkController

--- src/main/java/com/templateproject/api/controller/LinkController.java
+++ src/main/java/com/templateproject/api/controller/LinkController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Controller
