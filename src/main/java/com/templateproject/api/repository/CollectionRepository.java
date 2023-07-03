8ed3200790ecd219f12dc8f90a26ad8ec6b82f1b
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-07-03 10:17:19 +0200

    Create CollectionRepository

---
+++ src/main/java/com/templateproject/api/repository/CollectionRepository.java
@@ -0,0 +1,10 @@
+package com.templateproject.api.repository;
+
+import com.templateproject.api.entity.Collection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CollectionRepository extends JpaRepository<Collection, Long> {
+    
+}
