2520cd97842e6917523c32dc9160841a754b112d
cadart <cadart.valentin@sfr.fr>
2023-07-04 15:49:32 +0200

    add response when rssFeed not found in PUT method

--- src/main/java/com/templateproject/api/controller/RssFeedController.java
+++ src/main/java/com/templateproject/api/controller/RssFeedController.java
@@ -2,9 +2,12 @@ package com.templateproject.api.controller;
 
 import com.templateproject.api.entity.RssFeed;
 import com.templateproject.api.repository.RssFeedRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rssFeeds")
@@ -31,16 +34,20 @@ public class RssFeedController {
 
     @PutMapping("/{id}")
     public RssFeed update(@PathVariable int id, @RequestBody RssFeed rssFeed) {
-            RssFeed rssFeedToUpdate = rssFeedRepository.findById((long) id).get();
+        Optional<RssFeed> optionalFeedRss = rssFeedRepository.findById((long) id);
+        if(optionalFeedRss.isPresent()) {
+            RssFeed rssFeedToUpdate = optionalFeedRss.get();
             rssFeedToUpdate.setUrl(rssFeed.getUrl());
             rssFeedToUpdate.setFavorite(rssFeed.isFavorite());
             return rssFeedRepository.save(rssFeedToUpdate);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Rss Feed Not Found");
+        }
     }
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable int id) {
+    public @ResponseBody void delete(@PathVariable int id) {
         rssFeedRepository.deleteById((long) id);
-        return true;
     }
 
     @GetMapping("/favorites")
