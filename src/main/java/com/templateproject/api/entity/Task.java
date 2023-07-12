6ff1566877b0a1b2f55a747a228b092516985e34
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-07-12 10:39:12 +0200

    edit imports

--- src/main/java/com/templateproject/api/entity/Task.java
+++ src/main/java/com/templateproject/api/entity/Task.java
@@ -2,14 +2,7 @@ package com.templateproject.api.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Task {
