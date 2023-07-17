b10614d0efa67caaa6d4b8a06a74c8bbef298b2b
Emma Rosso <emmarosso.pro@gmail.com>
2023-07-17 14:24:50 +0200

    todos fav dashboard

--- src/main/java/com/templateproject/api/repository/ToDoListRepository.java
+++ src/main/java/com/templateproject/api/repository/ToDoListRepository.java
@@ -1,5 +1,7 @@
 package com.templateproject.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.templateproject.api.entity.ToDoList;
 @Repository
 public interface ToDoListRepository extends JpaRepository<ToDoList, Long>{
 
+  public List<ToDoList> findByIsFavorite(boolean isFavorite);
 }
