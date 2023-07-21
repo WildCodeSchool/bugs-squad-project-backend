264d3c34161e059ce386ea80b2eb9102cfdbffe8
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-07-21 10:57:04 +0200

    Update Dashboard.java

--- src/main/java/com/templateproject/api/entity/Dashboard.java
+++ src/main/java/com/templateproject/api/entity/Dashboard.java
@@ -1,13 +1,11 @@
 package com.templateproject.api.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Dashboard {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int xPos;
     private int yPos;
@@ -16,6 +14,10 @@ public class Dashboard {
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public int getxPos() {
