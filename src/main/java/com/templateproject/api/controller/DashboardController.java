c5f5f30dadef4962dde169cb1f9729aba3777674
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-07-21 10:16:52 +0200

    Update DashboardController.java

--- src/main/java/com/templateproject/api/controller/DashboardController.java
+++ src/main/java/com/templateproject/api/controller/DashboardController.java
@@ -2,9 +2,12 @@ package com.templateproject.api.controller;
 
 import com.templateproject.api.entity.Dashboard;
 import com.templateproject.api.repository.DashboardRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dashboard")
@@ -24,5 +27,24 @@ public class DashboardController {
     @PostMapping("")
     public @ResponseBody Dashboard createDashboard(@RequestBody Dashboard dashboard) {
         return dashboardRepository.save(dashboard);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Dashboard updateDashboard(@PathVariable(value = "id") Long id, @RequestBody Dashboard dashboard) {
+        Optional<Dashboard> optionalDashboard = dashboardRepository.findById(id);
+        if (optionalDashboard.isPresent()) {
+            Dashboard updatedDashboard = optionalDashboard.get();
+            updatedDashboard.setxPos(dashboard.getxPos());
+            updatedDashboard.setyPos(dashboard.getyPos());
+            updatedDashboard.setHeight(dashboard.getHeight());
+            updatedDashboard.setWidth(dashboard.getWidth());
+            return dashboardRepository.save(updatedDashboard);
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dashboard non trouvée");
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody void deleteDashboard(@PathVariable(value = "id") Long id) {
+        dashboardRepository.deleteById(id);
     }
 }
